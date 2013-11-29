e8e9ac3ae3f013130ba1edc2f80d486b0d068794
lrw <lrw@smw01.com>
2013-11-29 17:19:08 +0800

    更新数据配置

--- config.php
+++ config.php
@@ -2,18 +2,18 @@
  /*return array(
 	'DB_TYPE'=>'mysql',
 	'DB_HOST'=>'localhost',
-	'DB_NAME'=>'zend',
-	'DB_USER'=>'adminBS2PpxF',
-	'DB_PWD'=>'f2gAIA244j3M',
+	'DB_NAME'=>'uu156135',
+	'DB_USER'=>'uu156135',
+	'DB_PWD'=>'HxCzQBiX',
 	'DB_PORT'=>'3306',
 	'DB_PREFIX'=>'iic_',
 );*/
 return array(
 	'DB_TYPE'=>'mysql',
 	'DB_HOST'=>'localhost',
-	'DB_NAME'=>'hezheng_v2',
-	'DB_USER'=>'root',
-	'DB_PWD'=>'',
+	'DB_NAME'=>'zend',
+	'DB_USER'=>'adminBS2PpxF',
+	'DB_PWD'=>'f2gAIA244j3M',
 	'DB_PORT'=>'3306',
 	'DB_PREFIX'=>'iic_',
 );
